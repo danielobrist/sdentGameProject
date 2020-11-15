d2224b213b75bbf31e2717ad828a100a83f7e8cb
danielobrist <daniel.obrist@students.fhnw.ch>
2020-11-15 17:14:35 +0100

    control player torque with c and v keys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rbody;
+    public float torque;
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody>();
         rbody.AddForce(new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f)));
-        rbody.AddTorque(new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f)));
+        rbody.AddRelativeTorque(new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f)));
     }
 
 
@@ -20,22 +21,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey("w"))
         {
-            rbody.AddForce(new Vector3(0f, 1f, 0f)); 
+            rbody.AddForce(Vector3.up); 
         }
         if (Input.GetKey("s"))
         {
-            rbody.AddForce(new Vector3(0f, -1f, 0f));
+            rbody.AddForce(Vector3.down);
         }
         if (Input.GetKey("a"))
         {
-            rbody.AddForce(new Vector3(-1f, 0f, 0f));
+            rbody.AddForce(Vector3.left);
         }
         if (Input.GetKey("d"))
         {
-            rbody.AddForce(new Vector3(1f, 0f, 0f));
+            rbody.AddForce(Vector3.right);
         }
-
-
+        if (Input.GetKey("c"))
+        {
+            rbody.AddRelativeTorque(Vector3.up * 0.1f);
+        }
+        if (Input.GetKey("v"))
+        {
+            rbody.AddRelativeTorque(Vector3.down * 0.1f);
+        }
     }
+
 }
 
