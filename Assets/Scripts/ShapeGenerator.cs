85c24d75980763d1e62dc7d3d02eb265efe118f2
danielobrist <daniel.obrist@students.fhnw.ch>
2020-12-09 19:31:25 +0100

    parameterized sphere for procedural asteroid genration

---
+++ Assets/Scripts/ShapeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator
+{
+    ShapeSettings shapeSettings;
+
+    public ShapeGenerator(ShapeSettings settings)
+    {
+        this.shapeSettings = settings;
+    }
+
+    public Vector3 CalculatePointOnAsteroid(Vector3 pointOnUnitshpere)
+    {
+        return pointOnUnitshpere * shapeSettings.asteroidRadius;
+    }
+}
